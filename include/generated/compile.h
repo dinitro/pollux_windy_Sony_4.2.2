cf027da71b1a41c3595c2a0427f90ca13a1684cb
dinitro <aditjilla@hotmail.com>
2013-09-05 20:46:13 +0200

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 8 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#8 SMP PREEMPT Sun Sep 1 17:26:50 CEST 2013"
+#define LINUX_COMPILE_BY "adit"
+#define LINUX_COMPILE_HOST "ryu"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
